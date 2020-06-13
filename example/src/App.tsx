07140f702fbb9db4d66cfbf68b9fb53bde2e1b54
JonatanSalas <jonatan.salas@blackbox-vision.tech>
2020-06-13 08:34:35 -0300

    feat(tokenize-checkout): add initial implementation

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,18 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import ReactNativeMercadopagoTokenizeCheckout from '@blackbox-vision/react-native-mercadopago-tokenize-checkout';
+import { StatusBar, SafeAreaView } from 'react-native';
+import MercadoPagoWebTokenizeCheckout from '@blackbox-vision/react-native-mercadopago-tokenize-checkout';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    ReactNativeMercadopagoTokenizeCheckout.multiply(3, 7).then(setResult);
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <MercadoPagoWebTokenizeCheckout
+          amount={100.0}
+          action={'/payments'}
+          publicKey={'TEST-5d2cfefc-32ab-45d5-bab5-fc1bfc2bbb7c'}
+        />
+      </SafeAreaView>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,65 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
+import React, { useEffect, useRef } from 'react';
+import { WebView } from 'react-native-webview';
+import { BackHandler, ActivityIndicator } from 'react-native';
+
+export interface MercadoPagoWebTokenizeCheckoutProps {
+  amount: number;
+  action: string;
+  publicKey: string;
+  keepOpen?: boolean;
+  cardsIds?: string[];
+  customerId?: string;
+}
+
+const MercadoPagoWebTokenizeCheckout: React.FC<MercadoPagoWebTokenizeCheckoutProps> = ({
+  amount,
+  action,
+  keepOpen,
+  publicKey,
+}: MercadoPagoWebTokenizeCheckoutProps) => {
+  const ref = useRef(null);
+
+  const goBack: any = () => {
+    if (ref.current) {
+      (ref.current as any)?.back();
+    }
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', goBack);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', goBack);
+    };
+  }, [ref]);
+
+  return (
+    <WebView
+      ref={ref}
+      scalesPageToFit
+      javaScriptEnabled
+      startInLoadingState
+      keyboardDisplayRequiresUserAction
+      originWhitelist={['*']}
+      renderLoading={() => (
+        <ActivityIndicator color="black" size="large" style={{ flex: 1 }} />
+      )}
+      source={{
+        html: `
+          <form action="${action}" method="POST">
+            <script
+              src="https://www.mercadopago.com.ar/integrations/v1/web-tokenize-checkout.js"
+              data-transaction-amount="${amount}"
+              data-public-key="${publicKey}"
+              data-open="${keepOpen}"
+            >
+            </script>
+          </form>
+        `,
+      }}
+      style={{ flex: 1 }}
+    />
+  );
 };
+
+export default MercadoPagoWebTokenizeCheckout;
