0d726f726000cbc0a83248785e408851c833c7bc
Matias <mendozammatias@gmail.com>
2020-08-18 23:42:21 -0300

    chore: let example empty from data

--- example/src/screens/Payments.tsx
+++ example/src/screens/Payments.tsx
@@ -2,9 +2,6 @@ import * as React from 'react';
 import { useTheme } from 'react-native-paper';
 import MercadoPagoWebTokenizeCheckout from '@blackbox-vision/react-native-mercadopago-tokenize-checkout';
 
-// TODO: move to .env
-const PUBLIC_KEY = 'TEST-5d2cfefc-32ab-45d5-bab5-fc1bfc2bbb7c';
-
 export const PaymentsScreen = ({ route }: any) => {
   const { amount, title } = route.params;
   const { colors } = useTheme();
@@ -13,8 +10,14 @@ export const PaymentsScreen = ({ route }: any) => {
     <MercadoPagoWebTokenizeCheckout
       amount={amount}
       productLabel={title}
-      action="https://nervous-squid-21.serverless.social/payments"
-      publicKey={PUBLIC_KEY}
+      publicKey="MERCADO PAGO PUBLIC KEY"
+      action="BACKEND URL TO POST DATA"
+      successUrl="REDIRECT URL WHEN SUCCESS"
+      failureUrl="REDIRECT URL WHEN FAILURE"
+      onPaymentResult={(paymentData, error) => {
+        console.info('paymentData: ', JSON.stringify(paymentData, null, 2));
+        console.info('error: ', error);
+      }}
       theme={{
         elements: colors.primary,
         header: colors.primary,
